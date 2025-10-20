31830ecf09dc906f52ea48dec2d0bf8dd8095185
Michael McCusker <mjmcc1966@gmail.com>
2025-10-20 02:11:51 +0000

    and please change the 'upload custom questions the popular vote' to just

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                  <Button className="w-full h-20 text-xl justify-start" variant="outline">
                     <div className="flex items-center space-x-4">
                       <Upload className="w-6 h-6" />
-                      <span><div>Upload Custom Questions<br/>The Popular Vote</div></span>
+                      <span>Upload Custom Questions</span>
                     </div>
                   </Button>
                </UploadQuestionsDialog>
