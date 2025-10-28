61c0b9b18c5731a6ffd0e2ffc27acede6903064a
Michael McCusker <mjmcc1966@gmail.com>
2025-10-28 04:09:30 +0000

    Runtime Error
    FirebaseError: initializeFirestore() has already been ca

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,11 +1,10 @@
-
 'use client';
 
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
-import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { initializeFirestore, memoryLocalCache, Firestore }from 'firebase/firestore';
+import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
+import { getAuth, type Auth } from 'firebase/auth';
+import { initializeFirestore, memoryLocalCache, type Firestore } from 'firebase/firestore';
 import { firebaseConfig } from './config';
 
 interface FirebaseClientProviderProps {
