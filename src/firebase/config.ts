65b782c89466e23a429edf791083eb4cec041ab0
Michael McCusker <mjmcc1966@gmail.com>
2025-10-29 04:06:12 +0000

    feat: Secure API key using environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4440582707-11931",
   "appId": "1:265557677161:web:2edf88351e0987d7e77ef8",
-  "apiKey": "AIzaSyA3PXU3TCkjzg_6MazAfxpd7JRPivQ_D6Q",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-4440582707-11931.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "265557677161"
